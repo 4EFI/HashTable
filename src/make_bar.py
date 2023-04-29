5fbdda49ef4fa475c6e75fbe8d31955c5b799d16
4EFI <kvv204@gmail.com>
2023-04-29 15:47:29 +0300

    Draw all bars

--- src/make_bar.py
+++ src/make_bar.py
@@ -1,32 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig = plt.figure( figsize = (6, 4) )
-ax = fig.add_subplot()
+def DrawBar( file_name, x_size, y_size ):
+    
+    fig = plt.figure( figsize = (6, 4) )
+    ax = fig.add_subplot()
+    
+    data = []
 
-post_file_name = "crc32_avx"
+    with open( "res/output_" + file_name + ".csv", "r" ) as f:
+        data = list( map( int, f.readline().split() ) )
 
-data = []
+    x = [i for i in range( len( data ) )]
 
-with open( "res/output_" + post_file_name + ".csv", "r" ) as f:
-    data = list( map( int, f.readline().split() ) )
+    print( np.var( data ) )
 
-x = [i for i in range( len( data ) )]
+    plt.xlim  ( -5, x_size )
+    plt.ylim  (  0, y_size )
+    plt.xticks( [i for i in range( 0, x_size + 1, 100 )] )
+    plt.xlabel( "Индекс списка" )
+    plt.ylabel( "Количество элементов" )
 
-xmax = 1000
-ymax = 30
+    ax.bar( x, data, width = 1 )
+    plt.grid()
 
-print( np.var( data ) )
+    fig.savefig( "res/bar_" + file_name + ".png" )
 
-plt.xlim  ( -5, xmax )
-plt.ylim  (  0, ymax )
-plt.xticks( [i for i in range( 0, xmax + 1, 100 )] )
-plt.xlabel( "Индекс списка" )
-plt.ylabel( "Количество элементов" )
+x_size = 1001
+y_size = 30
 
-ax.bar( x, data, width = 1 )
-plt.grid()
+file_names = [ "const", "first_ascii", "len_word", "sum_ascii", "sum_ascii_5000", "rol",  "ror",  "bkdr", "crc32" ]
+x_sizes    = [ 200,     200,           200,        x_size,      5000,             x_size, x_size, x_size, x_size    ]
+y_sizes    = [ 6000,    1000,          1000,       y_size,      y_size,           y_size, y_size, y_size, y_size    ]
 
-fig.savefig( "res/bar_" + post_file_name + ".png" )
-
-plt.show()
+for i in range( len( file_names ) ):
+    DrawBar( file_names[i], x_sizes[i], y_sizes[i] )
